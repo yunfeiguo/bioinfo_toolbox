b3acb4c355781e6a3f41aa6ffc5e55856de009bf
Yunfei Guo <yunfei.guo@bina.roche.com>
2016-12-18 14:10:01 -0800

    add python

--- setup/install_dev_tools.sh
+++ setup/install_dev_tools.sh
@@ -3,7 +3,29 @@ cd $HOME/Downloads
 #gcc
 #cmake
 #zlib
-#python
+#python +setuptools+pip
+wget https://www.python.org/ftp/python/2.7.12/Python-2.7.12.tgz
+tar zxvf Python-2.7.12.tgz 
+pushd Python-2.7.12/
+./configure  --prefix=$HOME/Downloads/python-2.7.12_install
+make -j 6 && make install
+ln -sf $HOME/Downloads/python-2.7.12_install/bin/python $HOME/bin/
+
+wget https://github.com/pypa/setuptools/archive/v29.0.1.zip
+unzip v29.0.1.zip 
+pushd setuptools-29.0.1/
+python bootstrap.py 
+python setup.py install
+popd
+
+wget https://pypi.python.org/packages/11/b6/abcb525026a4be042b486df43905d6893fb04f05aac21c32c638e939e447/pip-9.0.1.tar.gz#md5=35f01da33009719497f01a4ba69d63c9
+tar zxvf pip-9.0.1.tar.gz 
+pushd pip-9.0.1/
+python setup.py install
+ln -sf $HOME/Downloads/python-2.7.12_install/bin/pip ~/bin/
+popd
+
+popd
 #perl
 #boost
 #asciidoc
@@ -35,3 +57,4 @@ make all doc
 make install install-doc install-html
 ln -sf $HOME/Downloads/git-2.11.0_install/bin/git ~/bin
 popd
+
