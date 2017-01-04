330f372f54fa1644b61b6366b8801785fdec8a2d
Yunfei Guo <yunfei.guo@bina.roche.com>
2017-01-04 10:20:09 -0800

    declaration and string

---
+++ sandbox/cpp/the_cpp_language_tutorial/string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string s ("x");
+    cout << "initial:";
+    cout << s << endl;
+    cout << "now enter yours:\n";
+    cin >> s;
+    cout << "you enetered:\n";
+    cout << s << endl;
+    return 0;
+}
