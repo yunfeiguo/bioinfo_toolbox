9e92ba47960dea979feb8974750809a00b1f2291
Yunfei Guo <yunfei.guo@bina.roche.com>
2017-01-06 17:32:51 -0800

    add example about getline

--- sandbox/cpp/the_cpp_language_tutorial/string.cpp
+++ sandbox/cpp/the_cpp_language_tutorial/string.cpp
@@ -9,5 +9,9 @@ int main() {
     cin >> s;
     cout << "you enetered:\n";
     cout << s << endl;
+    cout << "another input:\n";
+    getline (cin, s);
+    cout << "you enetered:\n";
+    cout << s << endl; //you can only get newline here, why? because previously cin stops at whitespace
     return 0;
 }
