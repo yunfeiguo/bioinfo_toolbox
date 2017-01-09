a66b6210eb0687ff0a9efdf22dc9fc280f08d824
Yunfei Guo <yunfei.guo@bina.roche.com>
2017-01-09 07:04:38 -0800

    function example

---
+++ sandbox/cpp/the_cpp_language_tutorial/function.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int addition(int a, int b) {
+    return a + b;
+}
+int main() {
+    int z (addition(3,2));
+    cout << "The result is " << z << endl;
+    return 0;
+}
