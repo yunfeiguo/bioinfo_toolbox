c48f432f66b27756f20bd83a227c8b0bda90e8da
Yunfei Guo <yunfei.guo@bina.roche.com>
2017-01-11 09:43:09 -0800

    pointer example

---
+++ sandbox/cpp/the_cpp_language_tutorial/function_pointer.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int addition(int x, int y) {
+    return x + y;
+}
+int subtract(int x, int y) {
+    return x - y;
+}
+int operation(int x, int y, int (*function)(int, int)) {
+	return (*function)(x, y);
+}
+int main() {
+    cout << "1 + 2 = " << operation(1, 2, addition) << endl;
+    cout << "1 - 2 = " << operation(1, 2, subtract) << endl;
+}
