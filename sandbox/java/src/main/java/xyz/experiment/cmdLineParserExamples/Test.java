417242be6b16c30b8ce15ccd1cfdcafb9681d340
Yunfei Guo <yunfei.guo@bina.roche.com>
2017-01-12 09:47:32 -0800

    commit old changes

--- sandbox/java/src/main/java/xyz/experiment/cmdLineParserExamples/Test.java
+++ sandbox/java/src/main/java/xyz/experiment/cmdLineParserExamples/Test.java
@@ -3,13 +3,14 @@ package experiment.cmdLineParserExamples;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
+
 /**
  * Created by guoy28 on 9/22/16.
  */
 public class Test {
 
     @Option(name = "-s" )
-    public Stock s;
+    public experiment.cmdLineParserExamples.Stock s;
 
     /**
      * try run it with command line arguments "-s aapl" and "-s xx"
