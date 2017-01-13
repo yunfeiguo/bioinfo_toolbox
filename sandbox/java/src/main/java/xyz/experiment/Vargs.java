a2e306449d39f2098a3de240651f61894ed1643b
Yunfei Guo <yunfei.guo@bina.roche.com>
2017-01-13 09:45:29 -0800

    try vargs

--- sandbox/java/src/main/java/xyz/experiment/Vargs.java
+++ sandbox/java/src/main/java/xyz/experiment/Vargs.java
@@ -4,4 +4,15 @@ package main.java.xyz.experiment;
  * Created by guoy28 on 1/13/17.
  */
 public class Vargs {
+  public static int sum(int... x) {
+    int sum = 0;
+    for (int i : x) {
+      sum += i;
+    }
+    return sum;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(sum(1,2,3,4));
+  }
 }
