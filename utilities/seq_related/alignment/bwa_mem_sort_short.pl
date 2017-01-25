0f15296427a0a3f932eb8afb5a9d6eafea57da7b
Yunfei Guo <yunfei.guo@bina.roche.com>
2017-01-25 10:38:18 -0800

    Create bwa_mem_sort_short.pl

---
+++ utilities/seq_related/alignment/bwa_mem_sort_short.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+
+die "Usage: $0 <ref prefix> <output prefix> <fq1> [fq2...]\n" unless @ARGV >= 3;
+
+$ref = shift @ARGV;
+$prefix = shift @ARGV;
+@fq = @ARGV;
+chomp(my $cpu_count = `grep -c -P '^processor\\s+:' /proc/cpuinfo`);
+$cpu_count *= 0.5; #use only 50% of CPU cores available
+$cpu_count++; #at least 1 CPU core
+
+!system("bwa mem -R '\@RG\\tID:$prefix.ID\\tSM:$prefix.SM' -t $cpu_count $ref @fq | samtools sort -o $prefix.sort.bam --output-fmt BAM -") or die "bwa or samtools failed: $!\n";
