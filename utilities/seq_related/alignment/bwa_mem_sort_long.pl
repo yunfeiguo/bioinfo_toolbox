a2d2ebb45cf8e3ac45d2cbb85d603dfe5d148806
Yunfei Guo <yunfei.guo@bina.roche.com>
2017-04-26 08:21:30 -0700

    bwa mem sort taking input BAM

--- utilities/seq_related/alignment/bwa_mem_sort_long.pl
+++ utilities/seq_related/alignment/bwa_mem_sort_long.pl
@@ -4,9 +4,29 @@ die "Usage: $0 <ref prefix> <output prefix> <fq1> [fq2...]\n" unless @ARGV >= 3;
 
 $ref = shift @ARGV;
 $prefix = shift @ARGV;
-@fq = @ARGV;
+@fq = grep {/\.(fastq|fq)$/} @ARGV;
+@bam = grep {/\.bam$/} @ARGV;
 chomp(my $cpu_count = `grep -c -P '^processor\\s+:' /proc/cpuinfo`);
 $cpu_count *= 0.5; #use only 50% of CPU cores available
 $cpu_count++; #at least 1 CPU core
 
-!system("bwa mem -R '\@RG\\tID:$prefix.ID\\tSM:$prefix.SM' -x pacbio -t $cpu_count $ref @fq | samtools sort -o $prefix.sort.bam --output-fmt BAM -") or die "bwa or samtools failed: $!\n";
+open ALIGN,"|-", "bwa mem -R '\@RG\\tID:$prefix.ID\\tSM:$prefix.SM' -x pacbio -t $cpu_count $ref /dev/stdin | samtools sort -o $prefix.sort.bam --output-fmt BAM -" or die "bwa or samtools failed: $!\n";
+if (@fq) {
+    open FQ,"-|","cat @fq" or die "failed to read fastqs: $!\n";
+    while(<FQ>) {
+	print ALIGN;
+    }
+    close FQ;
+}
+for my $bam(@bam) {
+    open BAM,"-|","bamToFastq -i $bam -fq /dev/stdout" or die "failed to read fastqs: $!\n";
+    while(<BAM>) {
+	print ALIGN;
+    }
+    close BAM;
+}
+close ALIGN;
+
+!system("samtools index $prefix.sort.bam") or die "samtools index: $!";
+
+##!system("cat @fq | bwa mem -R '\@RG\\tID:$prefix.ID\\tSM:$prefix.SM' -x pacbio -t $cpu_count $ref /dev/stdin | samtools sort -o $prefix.sort.bam --output-fmt BAM -") or die "bwa or samtools failed: $!\n";
