8b6e32f0cafb0c613073d4d1fa6dcdc32c0c075c
Yunfei Guo <yunfei.guo@bina.roche.com>
2017-07-23 18:27:12 -0700

    add build script

--- setup/dev_general/gcc.sh
+++ setup/dev_general/gcc.sh
@@ -1,1 +1,17 @@
-#
+#download the tarball, unzip it, configure, build and install it
+#create symbolic links in $HOME/bin
+#write env loading scripts for future loading
+wget -O- http://www.netgull.com/gcc/releases/gcc-5.4.0/gcc-5.4.0.tar.bz2 | tar jxvf -
+pushd gcc-5.4.0/
+./contrib/download_prerequisites
+popd
+mkdir -p gcc-5.4.0_build
+pushd gcc-5.4.0_build
+../gcc-5.4.0/configure --prefix=$HOME/Downloads/gcc-5.4.0_install \
+  --enable-shared --enable-threads --with-system-zlib --enable-languages=c,c++,fortran,go --disable-multilib
+  #make -j may have errors
+  make && make install
+ln -sf $HOME/Downloads/gcc-5.4.0_install/bin/gcc $HOME/bin
+ln -sf $HOME/Downloads/gcc-5.4.0_install/bin/g++ $HOME/bin
+popd
+
