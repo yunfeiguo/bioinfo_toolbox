d1684a85ed3fd5005951fe20e8f896ce1e0b0941
Yunfei Guo <yunfei.guo@bina.roche.com>
2017-07-26 16:31:08 -0700

    better debug logging

--- sandbox/python/boilerplate_code/python_opt_log.py
+++ sandbox/python/boilerplate_code/python_opt_log.py
@@ -10,7 +10,8 @@ logger = None
 def my_function(blah):
     return
 if __name__ == "__main__":
-    FORMAT = '%(levelname)s %(asctime)-15s %(name)-20s %(message)s'
+    INFO_FORMAT = '%(levelname)s %(asctime)-15s %(name)-20s %(message)s'
+    DEBUG_FORMAT = '%(levelname)s %(asctime)-15s %(name)-15s %(funcName)-20s %(message)s'
 
     parser = argparse.ArgumentParser(description="program name", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
@@ -20,13 +21,20 @@ if __name__ == "__main__":
     parser.add_argument("--cutoff", type = int, default = 1, help = 'cutoff score')
     parser.add_argument("--variable_args", type = float, action = 'append', nargs = 3, 
 	                default = [1.0,2.0,1.2], help = '3 scores')
+    parser.add_argument("--log_to_console", action = 'store_true', help = 'log to console instead of a file')
     parser.add_argument("--verbose","-v", action = 'count',  help='increase verbosity')
     args = parser.parse_args()
 
     if args.verbose >= 1:
-        logging.basicConfig(level=logging.DEBUG, format = FORMAT)
+        if args.log_to_console:
+            logging.basicConfig(level=logging.DEBUG, format = DEBUG_FORMAT)
+        else:
+            logging.basicConfig(level=logging.DEBUG, format = DEBUG_FORMAT, filename=args.prefix + '.log')
     else:
-        logging.basicConfig(level=logging.INFO, format=FORMAT)
+        if args.log_to_console:
+            logging.basicConfig(level=logging.INFO, format= INFO_FORMAT)
+        else:
+            logging.basicConfig(level=logging.INFO, format= INFO_FORMAT, filename=args.prefix + '.log')
     logger = logging.getLogger(__name__)
 
     logger.info("working hard ...")
