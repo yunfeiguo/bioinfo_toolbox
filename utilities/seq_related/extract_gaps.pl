7a9036b98954f74436660544083bd8df43cad7aa
Yunfei Guo <guoyunfei1989@gmail.com>
2019-04-19 21:51:49 -0700

    remove Bio::SeqIO dependency

--- utilities/seq_related/extract_gaps.pl
+++ utilities/seq_related/extract_gaps.pl
@@ -1,33 +1,51 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-use Bio::SeqIO;
 
 die "Usage: $0 <fasta>\n" unless @ARGV==1;
 warn "Scanning for gaps...\n";
 my $fa = shift;
-my $seqio_obj = Bio::SeqIO->new(-file=>$fa,-format=>'fasta');
 my $count = 0;
-while(my $i = $seqio_obj->next_seq) {
-    my $pos = 0; #position of probe
-    my $ingap = 0; #whether we are in a gap
-    for my $j(split //,$i->seq) {
-	if ($j eq 'N' or $j eq 'n') {
-	    print $i->id,"\t$pos\t" if $ingap == 0;
-	    $ingap = 1;
-	} else {
-	    if($ingap) {
-		$ingap = 0;
-		print $pos,"\tGap$count\n";
-		$count++;
-	    } else {
-		1;
-	    }
+open FA,"<",$fa or die "failed to open $fa: $!\n";
+my $id = "";
+my $seq = "";
+while(my $i=<FA>) {
+    if ($i =~ /^>/) {
+	if ($id) {
+	    process($id, $seq);
 	}
-	$pos++;
-    }
-    if($ingap) {
-	print $pos,"\tGap$count\n";
-	$count++;
-    }
+	($id) = $i =~ />(.*)/;
+	$seq = "";
+    } else {
+	chomp $i;
+	$seq .= $i;
+    }	
+}
+if ($id) {
+    process($id, $seq);
+}
+sub process {
+	#print "<<@_>>";
+	($id, $seq) = @_;
+    my $pos = 0; #position of probe
+	my $ingap = 0; #whether we are in a gap
+	for my $j(split //,$seq) {
+	    if ($j eq 'N' or $j eq 'n') {
+		print $id,"\t$pos\t" if $ingap == 0;
+		$ingap = 1;
+	    } else {
+		if($ingap) {
+		    $ingap = 0;
+		    print $pos,"\tGap$count\n";
+		    $count++;
+		} else {
+		    1;
+		}
+	    }
+    $pos++;
+}
+if($ingap) {
+    print $pos,"\tGap$count\n";
+    $count++;
+}
 }
